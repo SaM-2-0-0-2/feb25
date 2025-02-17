26775c8bdd8c0925496952836bca0af82705b1ae
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-02-17 14:20:11 +0530

    Add files via upload

---
+++ Logic Building Sessions/Day1/Test.java
@@ -0,0 +1,27 @@
+class Test{
+	public static void main(String args[]){
+		int a1;
+		int a = 10;
+		System.out.println("Value of a: " + a);
+		System.out.println("Sum of a: " + (a + a));
+			
+		byte A = (byte)131;
+		byte B = (byte)-129;
+		System.out.println("Value of byte A: " + A + "\tValue of byte B:" + B);
+			
+		short s = (byte)a;
+		System.out.println("Value of short variable: " + s);
+			
+		float f1 = 10.0f; //f is for float type casting
+		int f = (int)a;
+		System.out.println("Value of f1: " + f1 + "\tValue of f: " + f);
+
+		// float f2 = 10.0; //triggers error as java considers default decimal value as double
+		
+		double d = 10.0;
+		System.out.println("Value of double variable: " + d);
+	}
+}
+		
+		
+		
