5e15d4232ff929943da50d6ae739eea9452366bc
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-02-17 18:40:43 +0530

    Delete Logic Building Sessions/Assignment 1 directory

--- Logic Building Sessions/Assignment 1/Java Codes/Q5.java
+++
@@ -1,8 +0,0 @@
-class Q5 {
-    public static void main(String[] args) {
-        //Declaring variables and initializing thier values
-        int a = 25, b = 5;
-        //Displaying results
-        System.out.println(a + " X " + b + " = " + (a*b));
-    }    
-}
