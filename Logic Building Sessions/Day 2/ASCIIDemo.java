8edf01e049e96b9ea460f463b50203f7b8737cda
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-02-18 15:00:50 +0530

    Add files via upload

---
+++ Logic Building Sessions/Day 2/ASCIIDemo.java
@@ -0,0 +1,9 @@
+class ASCIIDemo {
+    public static void main(String[] args){
+        int a=255;
+        boolean b = false;
+        char c = 'z';
+        System.out.println((int)c);
+        System.out.println((char)a);
+    }    
+}
