d8600027120b9d9dda1186a05fb87ecf2bf31ed9
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-03-03 14:16:11 +0530

    Add files via upload

---
+++ OOPJ/Day 1/PRINT.java
@@ -0,0 +1,11 @@
+public class PRINT{
+	public static void main(String[] args){
+		System.out.println("Hello");
+		System.out.print("Hi");
+		System.out.println("Same Line");
+		System.out.println("New Line");
+		
+		double num = 100.23468562;
+		System.out.printf("Number = %.2f%n", num);
+	}
+}
