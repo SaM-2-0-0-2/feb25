99ff264978995a2fde239bc4688e758fa14eedf3
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-03-03 14:16:11 +0530

    Add files via upload

---
+++ OOPJ/Day 1/Demo1.java
@@ -0,0 +1,10 @@
+class ABC{
+	public static void main(String[] args){
+		System.out.println("ABC class");
+	}
+}
+class Demo1{
+	public static void main(String[] args){
+		System.out.println("Demo class");
+	}
+}
