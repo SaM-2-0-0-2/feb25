bde2fbaa3d9894ff9123ed517b1fb013d13769b5
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-03-05 00:25:25 +0530

    Add files via upload

---
+++ OOPJ/Day 2/HW/Q11.java
@@ -0,0 +1,13 @@
+public class Q11{
+	public static void main(String[] args){
+		int a=5;
+		int b=4;
+		if(a==5 || b==4){
+			System.out.println("A");	
+		} else if(a==6 && b==4){
+			System.out.println("B");	
+		} else if(a!=5){
+			System.out.println("C");	
+		}
+	}
+}
