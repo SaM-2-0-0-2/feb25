1100e53635c3ea8833ccd2f6164180bab99e9a50
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-03-06 00:39:39 +0530

    Delete OOPJ/Day 2/HW directory

--- OOPJ/Day 2/HW/Q02.java
+++
@@ -1,7 +0,0 @@
-public class Q02{
-	public static void main(String[] args){
-		int a = 54;
-		float f = a;
-		System.out.println(f);
-	}
-}
--- OOPJ/Day 2/HW/Q05.java
+++
@@ -1,8 +0,0 @@
-public class Q05{
-	public static void main(String[] args){
-		int x = 010;
-		int y = 52;
-		int z = 0x01F1;
-		System.out.println(x + " " + y + " " + z);
-	}
-}
