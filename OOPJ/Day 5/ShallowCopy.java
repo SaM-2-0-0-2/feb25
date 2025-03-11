9afbb6c5f8dbc562924ac46933dba66838e0f2c9
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-03-11 21:10:53 +0530

    Add files via upload

---
+++ OOPJ/Day 5/ShallowCopy.java
@@ -0,0 +1,30 @@
+class ShallowCopy {
+    private int[] data;
+
+    public ShallowCopy(int[] values) {
+        data = values; // Reference assignment
+    }
+
+    public void printData() {
+        for (int value: data) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3};
+        ShallowCopy obj1 = new ShallowCopy(values);
+        ShallowCopy obj2 = obj1; // Shallow Copy
+
+        obj1.printData();
+        obj2.printData();
+
+        values[0] = 10; // Modify original array
+
+        obj1.printData();
+        obj2.printData();
+    }
+}
