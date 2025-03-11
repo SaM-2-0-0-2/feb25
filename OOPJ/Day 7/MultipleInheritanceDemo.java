75a68d1b13039bc50d8eea1a6d555109162b0da8
Shriram Sabade <115067904+SaM-2-0-0-2@users.noreply.github.com>
2025-03-11 19:10:19 +0530

    Add files via upload

---
+++ OOPJ/Day 7/MultipleInheritanceDemo.java
@@ -0,0 +1,30 @@
+interface A{
+	default void msgA(){
+		System.out.println("A: Method of interface A");
+	}
+}
+
+interface B{
+	default void msgB(){
+		System.out.println("B: Method of interface B");
+	}
+}
+
+class C implements A, B{
+	public void msgC(){
+		System.out.println("C: Method of class C");
+	}
+	
+}
+
+
+class MultipleInheritanceDemo{
+	public static void main(String args[]){
+		
+		C c1  = new C();
+		c1.msgA();
+		c1.msgB();
+		c1.msgC();
+		
+	}	
+}
